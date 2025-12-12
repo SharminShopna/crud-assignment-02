f0b4be184c5272c14765649025c96d2a83ce45d7
Sharmin Akter <sharminshopna149@gmail.com>
2025-12-12 23:01:22 +0600

    added folder

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import express, { Request, Response } from "express";
+import {Pool} from "pg";
 const app = express()
 const port = 5000
+// parser
+app.use(express.json())
+// DB
+const pool = new Pool ({
 
+})
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World! sssssss')
+  res.send('Next Level CRUD Operation')
+})
+app.post ("/", (req: Request,res: Response) =>{
+  console.log(req.body);
+  res.status(201).json({
+    success: true,
+    message: "API is working",
+  })
 })
 
 app.listen(port, () => {
