a14dd0266b8dc8dc074a4ba20b8badb91468f7a8
Sharmin Akter <sharminshopna149@gmail.com>
2025-12-13 19:34:07 +0600

    added file

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,7 @@
-import express, { Request, Response } from "express";
-import {Pool} from "pg";
-const app = express()
-const port = 5000
-// parser
-app.use(express.json())
-// DB
-const pool = new Pool ({
+ import app from "./app";
+import config from "./config";
 
-})
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Next Level CRUD Operation')
-})
-app.post ("/", (req: Request,res: Response) =>{
-  console.log(req.body);
-  res.status(201).json({
-    success: true,
-    message: "API is working",
-  })
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+ const port = config.port;
+ app.listen(port, ()=>{
+  console.log(`Server is running on port ${port}`)
+ })
